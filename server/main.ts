dd93bbd9400020bb32f4bdbba88e7a38f646c12d
Gouransh <gouranshnayak1234@gmail.com>
2026-06-17 23:01:38 +0530

    typeScript intialised backend

--- server/main.ts
+++ server/main.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import type  {Request, Response, Application} from 'express';
 import MongoDb from './config/MongoDb.js';
+
 MongoDb();
-const app = express();
+
+const app: Application = express();
 app.use(express.json());
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response)=>{
     res.send('Hello from TypeScript');
 });
-app.listen(process.env.PORT, () => {
+
+app.listen(process.env.PORT, ()=>{
     console.log(`Server Running on Port: ${process.env.PORT}`);
-});
-//# sourceMappingURL=main.js.map
+})
