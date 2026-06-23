f6067e7d07b7d4dac1d1c32c3013e3471b1aec03
Gouransh <gouranshnayak1234@gmail.com>
2026-06-23 23:46:37 +0530

    getting ip address before metrix

--- server/src/scripts/metrix.ts
+++ server/src/scripts/metrix.ts
@@ -2,7 +2,36 @@ import os from 'os';
 import axios from 'axios';
 
 
-function getMetrix () {
+function getIPAdress() {
+    const interfaces = os.networkInterfaces();
+    let Ipv4: string | null = null;
+    let Ipv6: string | null = null;
+
+   for (const interfaceName in interfaces) {
+        const networkInterface = interfaces[interfaceName];
+        
+        if (!networkInterface) continue;
+
+        for (const network of networkInterface) {
+            if (network.internal) continue;
+
+            // Check for IPv4
+            if (network.family === 'IPv4' && !Ipv4) {
+                Ipv4 = network.address;
+            }
+            
+            // Check for IPv6
+            if (network.family === 'IPv6' && !Ipv6) {
+                Ipv6 = network.address;
+            }
+
+            // Break early if we found both
+            if (Ipv4 && Ipv6) break;
+        }
+    }
+
+}
+function getMetrix() {
     const freeMemory = os.freemem();
     const totalMemory = os.totalmem();
     const userMemory = totalMemory - freeMemory;
@@ -23,22 +52,22 @@ function getMetrix () {
     }
 }
 
-async function sendMetrix(payload:object) {
+async function sendMetrix(payload: object) {
     try {
-        const server = await axios.post('http://localhost:8080/api/metrix', payload, {withCredentials: true});
+        const server = await axios.post('http://localhost:8080/api/metrix', payload, { withCredentials: true });
         console.log(`[${new Date().toLocaleTimeString()}] Metrics successfully transmitted. Server responded with status: ${server.status}`);
     } catch (error: any) {
         console.log(error.message);
     }
 }
 
-function start(){
+function start() {
     //register the server first
-    setInterval(async()=>{
+    setInterval(async () => {
         console.log('sending the metrix')
         const payload = getMetrix();
-        
+
         await sendMetrix(payload);
-    },2500);
+    }, 2500);
 }
 start();
