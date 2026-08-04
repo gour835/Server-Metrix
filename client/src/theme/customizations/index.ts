4b11ce5322c2228a7699145be3eaf4dfcf7111ec
Gouransh <gouranshnayak1234@gmail.com>
2026-08-04 23:50:37 +0530

    mui setup

---
+++ client/src/theme/customizations/index.ts
@@ -0,0 +1,7 @@
+export { chartsCustomizations } from './charts';
+
+// @ts-expect-error hyst 
+export { dataGridCustomizations } from './dataGrid';
+export { datePickersCustomizations } from './datePickers';
+export { treeViewCustomizations } from './treeView';
+
