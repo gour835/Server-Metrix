3e86b48423952cfa31b4b12cd0fb72a7a2f84a0a
Gouransh <gouranshnayak1234@gmail.com>
2026-08-05 23:29:40 +0530

    empty sidebar initializes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,23 @@
 
 import './App.css'
 import { ChartExample } from './components/example-charts'
+import Layout from './layout'
 
 function App() {
   
 
   return (
     <>
-      <h1 className='text-zinc-950 text-4xl'>
-        Hello React
-      </h1>
-      <div className='w-125 h-125 '>
-      <ChartExample/>
+    <Layout>
+      <div className="flex flex-col gap-4 p-4">
+        <h1 className="text-2xl font-bold">Dashboard</h1>
+        
+        {/* Your chart container */}
+        <div className="w-full max-w-2xl rounded-xl border p-4 shadow-sm">
+          <ChartExample />
+        </div>
       </div>
+    </Layout>
     </>
   )
 }
