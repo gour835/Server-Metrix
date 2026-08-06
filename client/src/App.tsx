0c9a9c433aa126c1331b7051d881cb072bc83d3d
Gouransh <gouranshnayak1234@gmail.com>
2026-08-06 13:22:59 +0530

    toggle theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,38 +8,43 @@ import { SiteHeader } from './components/site-header'
 import { SidebarProvider, SidebarInset } from './components/ui/sidebar'
 import { TooltipProvider } from './components/ui/tooltip'
 import data from '../data.json'
+import { ThemeProvider } from './components/theme-provider'
 
 
 function App() {
-  
+
 
   return (
-   <TooltipProvider>
-      <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)",
-        } as React.CSSProperties
-      }
-    >
-      <AppSidebar variant="inset" />
-      <SidebarInset>
-        <SiteHeader />
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <SectionCards />
-              <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+
+      <TooltipProvider>
+        <SidebarProvider
+          style={
+            {
+              "--sidebar-width": "calc(var(--spacing) * 72)",
+              "--header-height": "calc(var(--spacing) * 12)",
+            } as React.CSSProperties
+          }
+        >
+          <AppSidebar variant="inset" />
+          <SidebarInset>
+            <SiteHeader />
+            <div className="flex flex-1 flex-col">
+              <div className="@container/main flex flex-1 flex-col gap-2">
+                <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                  <div className="px-4 lg:px-6">
+                    <ChartAreaInteractive />
+                  </div>
+                  <SectionCards />
+                  <SectionCards />
+                  <DataTable data={data} />
+                </div>
               </div>
-              <DataTable data={data} />
             </div>
-          </div>
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
-    </TooltipProvider>
+          </SidebarInset>
+        </SidebarProvider>
+      </TooltipProvider>
+    </ThemeProvider>
   )
 }
 
